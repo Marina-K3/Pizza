0ba07e4da31fcb3e1e6b9274ea0e7121fe82d7e1
Marina-K3 <99384382+Marina-K3@users.noreply.github.com>
2024-02-09 01:28:15 +0300

    status 404 resolved, front connected

--- src/main/java/pizza/config/StaticResourseConfiguration.java
+++ src/main/java/pizza/config/StaticResourseConfiguration.java
@@ -2,6 +2,7 @@ package pizza.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -14,6 +15,11 @@ public class StaticResourseConfiguration implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**")
                 .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+    }
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.jsp().prefix("/").suffix(".html");
     }
 }
 
